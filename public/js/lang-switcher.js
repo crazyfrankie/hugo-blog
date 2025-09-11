aaffd7efa69b005bcbc50475e7fc46c1f92b1f16
crazyfrank <axu9417@gmail.com>
2025-09-11 20:11:56 +0800

    fix:rm public

--- public/js/lang-switcher.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // 隐藏默认的语言切换器
-  const defaultLangSwitcher = document.querySelector('.menu-item.language');
-  if (defaultLangSwitcher) {
-    defaultLangSwitcher.style.display = 'none';
-  }
-});
